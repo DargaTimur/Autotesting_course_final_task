70a95916225673d11c6c8b40a2ceef4cbdbc8070
Timur <btesto010@gmail.com>
2023-05-01 23:43:52 +0800

    perform checks with assert and catching of exceptions

--- test_main_page.py
+++ test_main_page.py
@@ -9,4 +9,11 @@ def test_guest_can_go_to_login_page(browser):
     page.open_page() # открываем страницу
     page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open_page()
+    page.should_be_login_link()
+
+
 # pytest -v --tb=line --language=en c:\Users\Timur\Autotesting_course_final_task\test_main_page.py
