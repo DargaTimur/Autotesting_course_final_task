debc0a4702ea109daddb29a841c5eff5ccf7ac71
Timur <btesto010@gmail.com>
2023-05-01 19:26:59 +0800

    changed test_main_page and added pages folder

---
+++ pages/base_page.py
@@ -0,0 +1,10 @@
+import time
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open_page(self):
+        self.browser.get(self.url)
+        time.sleep(5)
